14dcdc21b030efee232c954d4efac06a57256f23
mkmDEV <mkmdev18@gmail.com>
2023-09-18 20:18:09 +0200

    base set up for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/food-order-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
